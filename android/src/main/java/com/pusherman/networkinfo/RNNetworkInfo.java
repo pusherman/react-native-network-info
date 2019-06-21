3b3cecb3de9a3192bd60b1e96bc30ecc46f53672
Richard V. Lam <richardvclam@gmail.com>
2019-06-21 08:44:14 -0700

    Fix missing import for Android
    Co-Authored-By: Chris <chris.odicho@gmail.com>

--- android/src/main/java/com/pusherman/networkinfo/RNNetworkInfo.java
+++ android/src/main/java/com/pusherman/networkinfo/RNNetworkInfo.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.Arrays;
 import java.util.List;
+import java.net.Inet6Address;
 
 public class RNNetworkInfo extends ReactContextBaseJavaModule {
     WifiManager wifi;
