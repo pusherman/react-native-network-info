c01b856e7ae434149c2ab4cc21af8a5fdc50d924
Isaac Parker <isaac.parker@opendoor.com>
2019-08-22 16:32:40 -0700

    Add iOS implementation

---
+++ ios/getgateway.h
@@ -0,0 +1,16 @@
+//
+//  getgateway.h
+//  OBDInterface
+//
+//  This is pulled directly from https://stackoverflow.com/a/29440193/1120802
+//
+
+#ifndef getgateway_h
+#define getgateway_h
+
+#include <stdio.h>
+
+int getdefaultgateway(in_addr_t * addr);
+
+#endif /* getgateway_h */
+
