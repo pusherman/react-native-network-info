8c9cd573f6a826ad24666be44a20012bb217ebfd
Isaac Parker <isaac.parker@opendoor.com>
2019-08-22 16:31:50 -0700

    Add definitions for default gateway IP

--- NetworkInfo.d.ts
+++ NetworkInfo.d.ts
@@ -5,5 +5,6 @@ export namespace NetworkInfo {
   function getIPAddress(): Promise<string | null>;
   function getIPV4Address(): Promise<string | null>;
   function getSubnet(): Promise<string | null>;
+  function getGatewayIPAddress(): Promise<string | null>;
   function getFrequency(): Promise<number | null>;
 }
