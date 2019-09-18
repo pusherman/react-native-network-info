920d2a0ca3d9475aa423c8b3b2c691aa2b251033
Rapsssito <rodrigo712000@gmail.com>
2019-09-18 12:19:15 +0200

    Updated with getWIFIIPV4Address

--- NetworkInfo.d.ts
+++ NetworkInfo.d.ts
@@ -4,6 +4,7 @@ export namespace NetworkInfo {
   function getBroadcast(): Promise<string | null>;
   function getIPAddress(): Promise<string | null>;
   function getIPV4Address(): Promise<string | null>;
+  function getWIFIIPV4Address(): Promise<string | null>;
   function getSubnet(): Promise<string | null>;
   function getGatewayIPAddress(): Promise<string | null>;
   function getFrequency(): Promise<number | null>;
